2181c50c93ad8f3cc45ed7b0b296fd43cc21f757
Niklas Kuder <nkqdr@users.noreply.github.com>
2022-05-29 11:44:39 +0200

    Added a test case for transactions page

---
+++ test/pages/transactions_page_test.dart
@@ -0,0 +1,27 @@
+import 'package:finance_buddy/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('User can open the "Add Transaction" sheet', (tester) async {
+    // Render app
+    await tester.pumpWidget(const MyApp(
+      initialTheme: ThemeMode.system,
+      shouldShowInstructions: false,
+      budgetOverflowEnabled: false,
+      monthlyLimit: 0,
+    ));
+    expect(find.text('Transactions'), findsOneWidget);
+
+    // Go to transactions page
+    await tester.tap(find.byIcon(Icons.compare_arrows_rounded));
+    await tester.pump();
+    expect(find.byIcon(Icons.add), findsOneWidget);
+
+    // Tap the add icon in the top right corner
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+    expect(find.text('Add transaction'), findsOneWidget);
+    expect(find.byType(ElevatedButton), findsNWidgets(3));
+  });
+}

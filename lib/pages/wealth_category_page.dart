efff44a4cd1d7e6761eb90938b720b92db6e7e42
Niklas Kuder <nkqdr@users.noreply.github.com>
2022-06-06 18:01:29 +0200

    Improved UX for wealth category page

--- lib/pages/wealth_category_page.dart
+++ lib/pages/wealth_category_page.dart
@@ -5,6 +5,7 @@ import 'package:monity/helper/utils.dart';
 import 'package:monity/widgets/adaptive_progress_indicator.dart';
 import 'package:monity/widgets/custom_appbar.dart';
 import 'package:monity/widgets/custom_cupertino_context_menu_action.dart';
+import 'package:monity/widgets/custom_section.dart';
 import 'package:monity/widgets/newmorphic/newmorphic_box.dart';
 import 'package:monity/widgets/view.dart';
 import 'package:flutter/cupertino.dart';
@@ -25,6 +26,7 @@ class WealthCategoryPage extends StatefulWidget {
 
 class _WealthCategoryPageState extends State<WealthCategoryPage> {
   late List<InvestmentSnapshot> snapshots;
+  late Map<int, List<InvestmentSnapshot>> snapshotsByDate;
   bool isLoading = false;
 
   @override
@@ -37,6 +39,14 @@ class _WealthCategoryPageState extends State<WealthCategoryPage> {
     setState(() => isLoading = true);
     snapshots = await FinancesDatabase.instance.readInvestmentSnapshotFor(widget.category.id!);
     snapshots.sort((a, b) => a.date.compareTo(b.date));
+    snapshotsByDate = {};
+    for (var snapshot in snapshots) {
+      if (snapshotsByDate.containsKey(snapshot.date.year)) {
+        snapshotsByDate[snapshot.date.year]!.add(snapshot);
+      } else {
+        snapshotsByDate[snapshot.date.year] = [snapshot];
+      }
+    }
     setState(() => isLoading = false);
   }
 
@@ -79,53 +89,82 @@ class _WealthCategoryPageState extends State<WealthCategoryPage> {
                   )
                 ]
               : [
-                  ...snapshots.reversed.map(
-                    (e) {
-                      return CupertinoContextMenu(
-                        actions: [
-                          CustomCupertinoContextMenuAction(
-                            child: Text(language.delete),
-                            trailingIcon: CupertinoIcons.delete,
-                            isDestructiveAction: true,
-                            onPressed: () => _handleDeleteSnapshot(e),
-                          )
-                        ],
-                        child: Padding(
-                          padding: const EdgeInsets.only(bottom: 15.0, left: 10, right: 10),
-                          child: NewmorphicBox(
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
-                              child: Material(
-                                color: Theme.of(context).scaffoldBackgroundColor,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      dateFormatter.format(e.date),
-                                      style: const TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    Text(
-                                      currencyFormat.format(e.amount),
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                        color: Theme.of(context).secondaryHeaderColor,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
+                  ...snapshotsByDate.keys
+                      .toList()
+                      .reversed
+                      .map(
+                        (year) => CustomSection(
+                          title: "$year",
+                          subtitle: _getPerformanceForYear(snapshotsByDate[year]!.first, snapshotsByDate[year]!.last),
+                          subtitleTextStyle: TextStyle(
+                            color: _calculatePerformance(snapshotsByDate[year]!.first, snapshotsByDate[year]!.last) >= 0
+                                ? Theme.of(context).hintColor
+                                : Theme.of(context).errorColor,
+                            fontWeight: FontWeight.bold,
                           ),
+                          children: [
+                            ...snapshotsByDate[year]!.reversed.map((snapshot) {
+                              return CupertinoContextMenu(
+                                actions: [
+                                  CustomCupertinoContextMenuAction(
+                                    child: Text(language.delete),
+                                    trailingIcon: CupertinoIcons.delete,
+                                    isDestructiveAction: true,
+                                    onPressed: () => _handleDeleteSnapshot(snapshot),
+                                  )
+                                ],
+                                child: Padding(
+                                  padding: const EdgeInsets.only(bottom: 15.0, left: 10, right: 10),
+                                  child: NewmorphicBox(
+                                    child: Container(
+                                      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                                      child: Material(
+                                        color: Theme.of(context).scaffoldBackgroundColor,
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              dateFormatter.format(snapshot.date),
+                                              style: const TextStyle(
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              currencyFormat.format(snapshot.amount),
+                                              style: TextStyle(
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.bold,
+                                                color: Theme.of(context).secondaryHeaderColor,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            }).toList(),
+                          ],
                         ),
-                      );
-                    },
-                  )
+                      )
+                      .toList()
                 ]),
     );
+  }
+
+  double _calculatePerformance(InvestmentSnapshot first, InvestmentSnapshot last) {
+    return (last.amount - first.amount) / first.amount;
+  }
+
+  String _getPerformanceForYear(InvestmentSnapshot first, InvestmentSnapshot last) {
+    double performance = _calculatePerformance(first, last);
+    String endString = "${(performance * 100).toStringAsFixed(1)}%";
+    if (performance > 0) {
+      return "+" + endString;
+    }
+    return endString;
   }
 
   Future _handleDeleteSnapshot(InvestmentSnapshot snapshot) async {
